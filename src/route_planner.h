9b52641fbac9d4d60aaf8d72889d9c5356ee0f80
saintograph <winfredselwyn@gmail.com>
2019-03-10 16:42:13 +0800

    Completed project tutorial.

--- src/route_planner.h
+++ src/route_planner.h
@@ -5,13 +5,23 @@
 #include <string>
 #include "route_model.h"
 
+class RoutePlanner
+{
+public:
+  RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y);
+  // Add public variables or methods declarations here.
+  float GetDistance() const { return distance; }
+  void AStarSearch();
 
-class RoutePlanner {
-  public:
-    RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y);
-    // Add public variables or methods declarations here.
-
-  private:
-    // Add private variables or methods declarations here.
-    RouteModel &m_Model;
+private:
+  // Add private variables or methods declarations here.
+  void AddNeighbors(RouteModel::Node *current_node);
+  float CalculateHValue(RouteModel::Node const *node);
+  std::vector<RouteModel::Node> ConstructFinalPath(RouteModel::Node *);
+  RouteModel::Node *NextNode();
+  RouteModel &m_Model;
+  RouteModel::Node *start_node;
+  RouteModel::Node *end_node;
+  float distance;
+  std::vector<RouteModel::Node *> open_list;
 };
